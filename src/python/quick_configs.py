1a4d46fe7866128d46ac1e419dbdf9e6775271fc
CadeMichael <cade.lueker@gmail.com>
2026-04-13 11:42:11 -0600

    file paths

--- src/python/quick_configs.py
+++ src/python/quick_configs.py
@@ -3,32 +3,32 @@ from netmiko import ConnectHandler
 import os
 
 def config_ospf(process_id, area, network, wildcard_mask):
-    with open('templates/ospf.j2', 'r') as template_file:
+    with open('src/python/templates/ospf.j2', 'r') as template_file:
         template = Template(template_file.read())
     return template.render(process_id=process_id, area=area, network=network, wildcard_mask=wildcard_mask)
 
 def config_ipv4_interface(name, ip_address, subnet_mask):
-    with open('templates/ipv4.j2', 'r') as template_file:
+    with open('src/python/templates/ipv4.j2', 'r') as template_file:
         template = Template(template_file.read())
     return template.render(name=name, ip_address=ip_address, subnet_mask=subnet_mask)
 
 def config_ipv6_interface(name, ipv6_address, prefix_length):
-    with open('templates/ipv6.j2', 'r') as template_file:
+    with open('src/python/templates/ipv6.j2', 'r') as template_file:
         template = Template(template_file.read())
     return template.render(name=name, ipv6_address=ipv6_address, prefix_length=prefix_length)
 
 def config_ipv4_route(destination, subnet_mask, next_hop):
-    with open('templates/ipv4_route.j2', 'r') as template_file:
+    with open('src/python/templates/ipv4_route.j2', 'r') as template_file:
         template = Template(template_file.read())
     return template.render(destination=destination, subnet_mask=subnet_mask, next_hop=next_hop)
 
 def config_ipv6_route(destination, prefix_length, next_hop):
-    with open('templates/ipv6_route.j2', 'r') as template_file:
+    with open('src/python/templates/ipv6_route.j2', 'r') as template_file:
         template = Template(template_file.read())
     return template.render(destination=destination, prefix_length=prefix_length, next_hop=next_hop)
 
 def config_bgp(as_number, neighbor_ip, remote_as):
-    with open('templates/bgp.j2', 'r') as template_file:
+    with open('src/python/templates/bgp.j2', 'r') as template_file:
         template = Template(template_file.read())
     return template.render(as_number=as_number, neighbor_ip=neighbor_ip, remote_as=remote_as)
 
